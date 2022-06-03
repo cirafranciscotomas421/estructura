0177aa5255b1d916165bcfc72cbbf1a6811aed04
unknown <cirafranciscotomas421@gmail.com>
2022-06-03 14:40:35 -0500

    actualizado 03 junio 2022

---
+++ operadores.py
@@ -0,0 +1,16 @@
+import math
+
+numero1=int(input("anota un numero entero "))
+numero2=float(input("anota otro numero "))
+suma=numero1+numero1
+print(suma)
+print(f"la suma de {numero1} mas {numero2} es igual a ",numero1+numero2)
+print(f"la resta de {numero1} menos {numero2} es igual a: ",numero1-numero2)
+print(f"la mulplicacion de{numero1} por {numero2} es igual a: ",numero1*numero2)
+print(f"la division de {numero1} / {numero2} es igual a:  ",numero1/numero2)
+print(f"la dision entera de {numero1} entre {numero2} es igual a: ",numero1/numero2)
+print(f" El residuo o modulo de la division de {numero1} entre {numero2} es igual a: ",numero1%numero2)
+#print("El resultado de la potenciacion es igual a: " ,math.pow(numero1,numero2)
+base=5
+eleva=2
+print("El resultado de la potenciacion es igual a:" , math.pow(base,eleva))
